f3ba38c3faddb9f9a88e1b66ccff75fc0bcea8bc
darryl1975 <darryl1975@hotmail.com>
2025-01-13 15:36:02 +0800

    CRUD for Book

---
+++ src/main/java/sg/edu/nus/iss/vttp5a_paf_day24/repository/BookRepo.java
@@ -0,0 +1,88 @@
+package sg.edu.nus.iss.vttp5a_paf_day24.repository;
+
+import java.beans.BeanProperty;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import sg.edu.nus.iss.vttp5a_paf_day24.model.Book;
+import sg.edu.nus.iss.vttp5a_paf_day24.utils.Query;
+
+@Repository
+public class BookRepo {
+    
+    @Autowired
+    JdbcTemplate template;
+
+    public Boolean insertBook(Book book) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        PreparedStatementCreator psc = new PreparedStatementCreator() {
+
+            @Override
+            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                PreparedStatement ps = con.prepareStatement(Query.createBookSql, new String[] {"id"});
+                ps.setString(1, book.getTitle());
+                ps.setInt(2, book.getQuantity());
+
+                return ps;
+            }
+            
+        };
+
+        int createdBookId = template.update(psc, keyHolder);
+
+        if (createdBookId > 0)
+            return true;
+        return false;
+    }
+
+    public List<Book> getAllBooks() {
+        List<Book> books = template.query(Query.getBooksSql, BeanPropertyRowMapper.newInstance(Book.class));
+
+        if (books.isEmpty()) {
+            // throw new BookNotfounException(".....");
+        }
+
+        return books;
+    }
+
+    public Book getBookById(int bookId) {
+        Book book =  null;
+        
+        try {
+            book = template.queryForObject(Query.getBookById, BeanPropertyRowMapper.newInstance(Book.class), bookId);
+        } catch (DataAccessException ex) {
+            // throw new BookNotFoundException("....");
+        }
+
+
+        return book;
+    }
+
+    public Boolean updateBook(Book book) {
+        int bUpdated = template.update(Query.updateBookById, book.getTitle(), book.getQuantity(), book.getId());
+
+        if (bUpdated > 0)
+            return true;
+        return false;
+    }
+
+    public Boolean updateBookStatus(Book book) {
+        int bUpdated = template.update(Query.updateBookStatusById, book.getIsActive(), book.getId());
+
+        if (bUpdated > 0)
+            return true;
+        return false;
+    } 
+}
---
+++ src/main/java/sg/edu/nus/iss/vttp5a_paf_day24/restcontroller/BookController.java
@@ -0,0 +1,61 @@
+package sg.edu.nus.iss.vttp5a_paf_day24.restcontroller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import sg.edu.nus.iss.vttp5a_paf_day24.model.Book;
+import sg.edu.nus.iss.vttp5a_paf_day24.repository.BookRepo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+    
+    @Autowired
+    BookRepo bookRepo;
+
+    @GetMapping("")
+    public ResponseEntity<List<Book>> getBooks() {
+        return ResponseEntity.ok().body(bookRepo.getAllBooks());
+    }
+
+    @GetMapping("/{bookid}")
+    public ResponseEntity<Book> getBookById(@PathVariable("book-id") Integer bookId) {
+        return ResponseEntity.ok().body(bookRepo.getBookById(bookId));
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Boolean> createBook(@RequestBody Book book) {
+        Boolean bCreated = bookRepo.insertBook(book);
+        
+        return ResponseEntity.ok().body(bCreated);
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<Boolean> updateBook(@PathVariable("id") Integer id, @RequestBody Book book) {
+        Boolean bUpdated = bookRepo.updateBook(book);
+        
+        return ResponseEntity.ok().body(bUpdated);
+    }
+    
+    @PutMapping("/status/{id}")
+    public ResponseEntity<Boolean> updateBookStatus(@PathVariable("id") Integer id, @RequestBody Book book) {
+        Boolean bUpdated = bookRepo.updateBookStatus
+        (book);
+        
+        return ResponseEntity.ok().body(bUpdated);
+    }
+    
+}
